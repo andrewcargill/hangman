2604091df05e9e03406bc151bf89ff5ed56027f1
andrewcargill <andycargill01@aol.com>
2022-09-30 13:24:13 +0000

    Initial Commit

--- run.py
+++ run.py
@@ -1,3 +1,56 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+HANGMAN = ['''
+        _ _ _ _
+       |       
+       |       
+       |      
+       |      
+       |      
+       |      
+    #==#==#''', '''
+        _ _ _ _
+       |       |
+       |       0
+       |       
+       |      
+       |      
+       |     
+    #==#==#''', '''
+        _ _ _ _
+       |       |
+       |       0
+       |       | 
+       |       |
+       |      
+       |     
+    #==#==#''', '''
+        _ _ _ _
+       |       |
+       |       0
+       |      /|\ 
+       |       |
+       |       
+       |     
+    #==#==#''', '''
+        _ _ _ _
+       |       |
+       |       0
+       |      /|\ 
+       |       |
+       |      / 
+       |     -   
+    #==#==#''', '''
+        _ _ _ _
+       |       |
+       |       0
+       |      /|\ 
+       |       |
+       |      / \ 
+       |     -   - 
+    #==#==#''']
+
+for x in range(len(HANGMAN)):
+    print(HANGMAN[x])
