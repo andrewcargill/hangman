e819295a4771752b70808bafe191c402dc27ea04
andrewcargill <andycargill01@aol.com>
2022-09-30 13:45:23 +0000

    Create opening graphic and hangmen

--- run.py
+++ run.py
@@ -54,3 +54,30 @@ HANGMAN = ['''
 
 for x in range(len(HANGMAN)):
     print(HANGMAN[x])
+
+GRAPHIC = '''
+—————————————————————————————————————————————————————
+
+HHH      HHH      AAA      NNN       NNN      GGGGG
+HHH      HHH    AAA AAA    NNN N     NNN    GGG    GGG
+HHH      HHH   AAA   AAA   NNN  N    NNN  GGG
+HHHHHHHHHHHH  AAAAAAAAAAA  NNN   N   NNN  GGG    
+HHH      HHH  AAA     AAA  NNN    N  NNN  GGG    GGGGG
+HHH      HHH  AAA     AAA  NNN     N NNN   GGG     GGG
+HHH      HHH  AAA     AAA  NNN      NNNN    GGGGGGG
+
+MMM      MMM      AAA      NNN       NNN     _ _ _ _
+MMMM    MMMM    AAA AAA    NNN N     NNN    |       |
+MMM M  M MMM   AAA   AAA   NNN  N    NNN    |       0
+MMM  MM  MMM  AAAAAAAAAA   NNN   N   NNN    |      /|\  
+MMM      MMM  AAA    AAA   NNN    N  NNN    |       |
+MMM      MMM  AAA    AAA   NNN     N NNN    |      / \ 
+MMM      MMM  AAA    AAA   NNN.     NNNN    |     -   -  
+
+
+ ——————————————CREATED BY ANDREW CARGILL———————————
+ 
+ 
+ '''
+
+print(GRAPHIC)
