334a6f81ab9b437ed4426e7f50b97abad85a31c5
andrewcargill <andycargill01@aol.com>
2022-10-01 19:29:20 +0000

    Update and feedback on first guess

--- run.py
+++ run.py
@@ -114,20 +114,64 @@ def word_display(x):
         guess_list.pop()
     return guess_list
  
+ 
+ # if letter is in word 
+    ## message to user "Nice! X is a great guess"
+    ## Update the display_word
+    ## show same 'hangman'
+# else if letter is not in word
+    ## message to user "Unlucky, thats not in word"
+    ## add a step to the hangman 
+    ## add the letter to the wrong guesses 
+
+#Then - ask for a new letter
+
+
 def user_guess(guess, answer, display_word):
-    guess = guess.lower()  #Players guess letter
-    letter_index = answer.index(guess) #find index number of letter in word
-    display_word[letter_index] = guess #updates the display_word
+    guess = guess.lower()  #Players guess (letter)
     
-    print(guess) #Testing: index of letters in word
-    print("TEST: you guessed: " + guess) #testing: letter guessed
-    print(answer) #testing: the word
-    print(display_word) #testing: the current display
+    if guess in answer:
+        print("-=-=-=-=-=-=-=-=-=-=-=-=-")
+        print(HANGMAN[0])
+        print("-=-=        =-=-=-=-=-=-=-")
+        print("\n")
+        print("Yes! '" + guess.upper()+ "' was a great guess!")
+        letter_index = answer.index(guess) #find index number of letter in word (integer)
+        display_word[letter_index] = guess.upper() #updates the display_word (list)
+        print("\n")
+        print(display_word)
+        print("\n")
+        print("wrong guesses: ") ### this needs to be a list
+        print("\n")
+        next_guess = input("...Give me another letter to try!: ")
+    else:
+        print("-=-=-=-=-=-=-=-=-=-=-=-=-")
+        print(HANGMAN[1]) # This needs to automatically add 1 each wrong answer
+        print("-=-=        =-=-=-=-=-=-=-")
+        print("\n")
+        print("Sorry '" + guess.upper() + "' isn't in the word!")
+        print("\n")
+        print(display_word)
+        print("\n")
+        print("wrong guesses: " + guess.upper()) ### this needs to be a list
+        print("\n")
+        next_guess = input("Have another guess: ")
+        print()
+    """
+    if bool(letter_index) == True:
+        print("unlucky, try again")
+        display_word[letter_index] = guess #updates the display_word (list)
+    else:
+        print("The letter " + guess + " was in the word!")
+        display_word[letter_index] = guess #updates the display_word (list)
+    #print(guess) #Testing: index of letters in word
+    #print(answer) #testing: the word
+    #print(display_word) #testing: the current display
+   """ 
 
 ## Start game input   
 def game_start_input():
     while True:
-
         game_start = input("To start: Enter 'E' for easy or 'H' for hard: ")
 
         if game_start.lower() == "h":
@@ -144,10 +188,10 @@ def load_game():
     """
     1. #### A function that generates a word in 'magic_word'
     1a.#### function that generates guess display word from magic word
-    2. A function that listens to the 'guess'
+    2. ###A function that listens to the 'guess'
     3. A function that steps the index of 'graphic' if wrong
     4. A function that updates 'wrong_guesses' with wrong letters
-    5. A function that updates 'magic_word'
+    5. ###A function that updates 'magic_word'
     """
     random_word()
 
@@ -159,8 +203,8 @@ def load_game():
     word_text = "Here's the word to guess:    "
     magic_word = magic_word #for testing
     print("TESTING this is the magic_word:  " + magic_word) #for testing
-    print(magic_word_list) #for testing (this is a list of the word)
-    print(magic_word_length) #for testing (this is how many characters)
+    #print(magic_word_list) #for testing (this is a list of the word)
+    #print(magic_word_length) #for testing (this is how many characters)
     print(line_break + word_text + line_break)
     word = word_display(magic_word_length) #for testing (word displayed)
     print(word)
